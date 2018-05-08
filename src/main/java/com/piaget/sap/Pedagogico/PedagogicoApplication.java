19544f96c5d7dbf3898a08909e5e577716def85a
ShadowTapia <chelo.tapia@gmail.com>
2018-05-08 00:04:00 -0300

    Mi primer commit

---
+++ src/main/java/com/piaget/sap/Pedagogico/PedagogicoApplication.java
@@ -0,0 +1,12 @@
+package com.piaget.sap.Pedagogico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedagogicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedagogicoApplication.class, args);
+	}
+}
